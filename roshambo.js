275d14081375efd52c3f21d98ffdad2cced81a24
jepjep-rgb <jefroyalde@gmail.com>
2022-07-24 13:46:21 +0800

    Test computer choice function and link js file to index.html
    Test getcomputerChoice function by letting it run 100 times. This is
    to check if the random number variable is statistically equivalent
    to one-third for each choice. The test was run after the javascript
    file is linked to the main index.html file.

--- roshambo.js
+++ roshambo.js
@@ -1,14 +1,27 @@
-
+let rock = 0;
+let paper = 0;
+let scissors = 0; 
 
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random()*3);
     if (compChoice === 0){
         console.log("Rock");
+        rock++;
     } else if (compChoice === 1) {
         console.log("Paper");
+        paper++;
     } else if (compChoice === 2) {
         console.log("Scissors");
+        scissors++;
     } else {
         alert("Error");
     }
 }
+
+for (i = 0; i < 100; i++){
+    getComputerChoice();
+}
+
+console.log(rock);
+console.log(paper);
+console.log(scissors);
