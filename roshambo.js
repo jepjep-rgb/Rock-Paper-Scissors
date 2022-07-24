bfdc5154881400ca2bce934003ca6e292ac3fca5
jepjep-rgb <jefroyalde@gmail.com>
2022-07-24 14:29:32 +0800

    Update computer choice function and remove test expressions
    Update the getComputerChoice function to return string values.
    
    Remove the variables and loop that tests the getComputerChoice
    function capabilities.

--- roshambo.js
+++ roshambo.js
@@ -1,30 +1,15 @@
-let rock = 0;
-let paper = 0;
-let scissors = 0; 
-
 function getComputerChoice() {
     let compChoice = Math.floor(Math.random()*3);
     if (compChoice === 0){
-        console.log("Rock");
-        rock++;
+        return "Rock";
     } else if (compChoice === 1) {
-        console.log("Paper");
-        paper++;
+        return "Paper";
     } else if (compChoice === 2) {
-        console.log("Scissors");
-        scissors++;
+        return "Scissors";
     } else {
         alert("Error");
     }
 }
-
-for (i = 0; i < 100; i++){
-    getComputerChoice();
-}
-
-console.log(rock);
-console.log(paper);
-console.log(scissors);
 
 function playRound(playerSelection, computerSelection){
     let ps = playerSelection.toLowerCase();
